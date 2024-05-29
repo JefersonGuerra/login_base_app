7c78b827c6f66ed40b81a851da7e6d5dab9cad2d
JefersonGuerra <jeferson.ctbbr@gmail.com>
2024-05-29 20:51:13 -0300

    add nativewind/types

--- env.d.ts
+++ env.d.ts
@@ -1,3 +1,4 @@
+/// <reference types="nativewind/types" />
 declare module '@env' {
     export const API_BASEURL: string
 }
--- src/Services/Api.tsx
+++ src/Services/Api.tsx
@@ -5,8 +5,4 @@ const api = axios.create({
     baseURL: API_BASEURL
 });
 
-const apiBug = axios.create({
-    baseURL: 'https://api1.nsctotal.com.br/api'
-});
-
-export {api, apiBug};
+export {api};
